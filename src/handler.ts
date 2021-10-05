159cc478841e9ebd83f568341658fe1f5df687cf
Frédéric Collonval <fcollonval@gmail.com>
2021-10-05 17:31:42 +0200

    First backend version

--- src/handler.ts
+++ src/handler.ts
@@ -17,7 +17,7 @@ export async function requestAPI<T>(
   const settings = ServerConnection.makeSettings();
   const requestUrl = URLExt.join(
     settings.baseUrl,
-    'search-replace', // API Namespace
+    'search-regex', // API Namespace
     endPoint
   );
 
