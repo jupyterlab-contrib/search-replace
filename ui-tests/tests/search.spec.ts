ffe7b08acf94ff2edbb2e76314e027d4fef20cbd
Madhur Tandon <madhur16053@iiitd.ac.in>
2022-03-07 20:19:29 +0530

    add basic search test with playwright

---
+++ ui-tests/tests/search.spec.ts
@@ -0,0 +1,25 @@
+import { test } from '@jupyterlab/galata';
+import { expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
+  await page.locator('#tab-key-0 .lm-TabBar-tabIcon svg').first().click();
+  // Click input[type="search"]
+  await page.locator('input[type="search"]').click();
+  // Fill input[type="search"]
+  await page.locator('input[type="search"]').fill('strange');
+
+  await Promise.all([
+    page.waitForResponse(
+      response =>
+        /.*search\/\?query=strange/.test(
+          response.url()
+        ) && response.request().method() === 'GET'
+    ),
+    page.locator('input[type="search"]').press('Enter')
+  ]);
+
+  expect(
+    await page.waitForSelector(`#jp-search-replace >> text=${JSON.stringify({"matches":[]}, undefined, 4)}`)
+  ).toBeTruthy();
+});
