c77763b51d8ec25e40bdc33b03facfcb834ed31e
Madhur Tandon <madhur16053@iiitd.ac.in>
2022-03-10 16:48:14 +0530

    add tests for tree view

--- ui-tests/tests/search.spec.ts
+++ ui-tests/tests/search.spec.ts
@@ -1,11 +1,26 @@
-import { test } from '@jupyterlab/galata';
+import { test, galata } from '@jupyterlab/galata';
 import { expect } from '@playwright/test';
+import * as path from 'path';
 
-test('test', async ({ page }) => {
+const fileName = 'conftest.py';
+test.use({ tmpPath: 'search-replace-test' });
+
+test.beforeAll(async ({ baseURL, tmpPath }) => {
+  const contents = galata.newContentsHelper(baseURL);
+  await contents.uploadFile(
+    path.resolve(__dirname, `../../jupyterlab_search_replace/tests/${fileName}`),
+    `${tmpPath}/${fileName}`
+  );
+});
+
+test.afterAll(async ({ baseURL, tmpPath }) => {
+  const contents = galata.newContentsHelper(baseURL);
+  await contents.deleteDirectory(tmpPath);
+});
+
+test('should get 5 matches', async ({ page }) => {
   // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
   await page.locator('[title="Search and replace"]').click();
-  // Click input[type="search"]
-  await page.locator('input[type="search"]').click();
   // Fill input[type="search"]
   await page.locator('input[type="search"]').fill('strange');
 
@@ -13,6 +28,32 @@ test('test', async ({ page }) => {
     page.waitForResponse(
       response =>
         /.*search\/\?query=strange/.test(
+          response.url()
+        ) && response.request().method() === 'GET'
+    ),
+    page.locator('input[type="search"]').press('Enter')
+  ]);
+
+  await page.locator('.expand-collapse-button').click();
+
+  expect(
+    await page.waitForSelector('jp-tree-view[role="tree"] >> text=5')
+  ).toBeTruthy();
+
+  await expect(page.locator('jp-tree-item:nth-child(4)')).toHaveText('                "Unicode strange sub file, very strange",');
+});
+
+
+test('should get no matches', async ({ page }) => {
+  // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
+  await page.locator('[title="Search and replace"]').click();
+  // Fill input[type="search"]
+  await page.locator('input[type="search"]').fill('dhit');
+
+  await Promise.all([
+    page.waitForResponse(
+      response =>
+        /.*search\/\?query=dhit/.test(
           response.url()
         ) && response.request().method() === 'GET'
     ),
