73f58f9b7cda72585ce69eeeeae0757a3a199b63
Madhur Tandon <madhur16053@iiitd.ac.in>
2022-04-08 01:23:31 +0530

    add replace per match and replaceAll per file

--- src/icon.ts
+++ src/icon.ts
@@ -4,6 +4,7 @@ import wholeWord from '../style/icons/whole-word.svg';
 import expandAll from '../style/icons/expand-all.svg';
 import collapseAll from '../style/icons/collapse-all.svg';
 import replaceAll from '../style/icons/replace-all.svg';
+import replace from '../style/icons/replace.svg';
 
 export const wholeWordIcon = new LabIcon({
   name: 'search-replace:wholeWord',
@@ -23,4 +24,9 @@ export const collapseAllIcon = new LabIcon({
 export const replaceAllIcon = new LabIcon({
   name: 'search-replace:replaceAll',
   svgstr: replaceAll
+});
+
+export const replaceIcon = new LabIcon({
+  name: 'search-replace:replace',
+  svgstr: replace
 });
