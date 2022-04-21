6cb5a90f0a9cc161bfd203aefd8fb7cae89b36ad
Frédéric Collonval <fcollonval@gmail.com>
2022-04-21 14:32:46 +0200

    Fix selector for integration tests

--- ui-tests/tests/replacePerItem.spec.ts
+++ ui-tests/tests/replacePerItem.spec.ts
@@ -30,117 +30,144 @@ test.afterAll(async ({ baseURL, tmpPath }) => {
 });
 
 test('should replace results for a particular file only', async ({ page }) => {
-    // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
-    await page.locator('[title="Search and replace"]').click();
-    // Fill input[type="search"]
-    await page.locator('input[type="search"]').fill('strange');
-  
-    await Promise.all([
-      page.waitForResponse(
-        response =>
+  // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
+  await page.locator('[title="Search and Replace"]').click();
+  // Fill input[type="search"]
+  await page.locator('input[type="search"]').fill('strange');
+
+  await Promise.all([
+    page.waitForResponse(
+      response =>
         /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
-          response.request().method() === 'GET'
-      ),
-      page.locator('input[type="search"]').press('Enter'),
-      page.waitForSelector('.jp-search-replace-tab >> .jp-progress', {
-        state: 'hidden'
-      })
-    ]);
-  
-    expect(
-      await page.waitForSelector('jp-tree-view[role="tree"] >> text=5')
-    ).toBeTruthy();
-  
-    await page.waitForSelector('.search-tree-files:has-text("conftest.py") >> .search-tree-matches:has-text(\'                "Is that Strange enough?",\')');
-  
-    await page.locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]').click();
-    await page.locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]').fill('hello');
+        response.request().method() === 'GET'
+    ),
+    page.locator('input[type="search"]').press('Enter'),
+    page.waitForSelector('.jp-search-replace-tab >> .jp-progress', {
+      state: 'hidden'
+    })
+  ]);
 
-    // press replace all matches for `conftest.py` only
-    await page.locator('.search-tree-files:has-text("conftest.py") >> [title="button to replace a results from a particular file"]').click();
+  expect(
+    await page.waitForSelector('jp-tree-view[role="tree"] >> text=5')
+  ).toBeTruthy();
 
-    // new results for previous query 'strange' should only have `test_handlers.py`
-    await page.waitForTimeout(800);
-    expect(await page.locator('.search-tree-files').count()).toEqual(1);
-    expect(await page.waitForSelector('jp-tree-view[role="tree"] >> text=test_handlers.py')).toBeTruthy();
-  
-    // new search with `hello`
-    await page.locator('input[type="search"]').fill('hello');
-    await Promise.all([
-      page.waitForResponse(
-        response =>
+  await page.waitForSelector(
+    '.search-tree-files:has-text("conftest.py") >> .search-tree-matches:has-text(\'                "Is that Strange enough?",\')'
+  );
+
+  await page
+    .locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]')
+    .click();
+  await page
+    .locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]')
+    .fill('hello');
+
+  // press replace all matches for `conftest.py` only
+  await page
+    .locator(
+      '.search-tree-files:has-text("conftest.py") >> [title="button to replace a results from a particular file"]'
+    )
+    .click();
+
+  // new results for previous query 'strange' should only have `test_handlers.py`
+  await page.waitForTimeout(800);
+  expect(await page.locator('.search-tree-files').count()).toEqual(1);
+  expect(
+    await page.waitForSelector(
+      'jp-tree-view[role="tree"] >> text=test_handlers.py'
+    )
+  ).toBeTruthy();
+
+  // new search with `hello`
+  await page.locator('input[type="search"]').fill('hello');
+  await Promise.all([
+    page.waitForResponse(
+      response =>
         /.*search\/[\w-]+\?query=hello/.test(response.url()) &&
-          response.request().method() === 'GET'
-      ),
-      page.locator('input[type="search"]').press('Enter'),
-      page.waitForSelector('.jp-search-replace-tab >> .jp-progress', {
-        state: 'hidden'
-      })
-    ]);
-  
-    // verify if `conftest.py` has changed
-    expect(
-      await page.waitForSelector('jp-tree-view[role="tree"] >> text=5')
-    ).toBeTruthy();
-  
-    await page.waitForSelector('.search-tree-files:has-text("conftest.py") >> .search-tree-matches:has-text(\'                "Is that hello enough?",\')');
+        response.request().method() === 'GET'
+    ),
+    page.locator('input[type="search"]').press('Enter'),
+    page.waitForSelector('.jp-search-replace-tab >> .jp-progress', {
+      state: 'hidden'
+    })
+  ]);
+
+  // verify if `conftest.py` has changed
+  expect(
+    await page.waitForSelector('jp-tree-view[role="tree"] >> text=5')
+  ).toBeTruthy();
+
+  await page.waitForSelector(
+    '.search-tree-files:has-text("conftest.py") >> .search-tree-matches:has-text(\'                "Is that hello enough?",\')'
+  );
 });
 
 test('should replace results for a particular match only', async ({ page }) => {
-    // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
-    await page.locator('[title="Search and replace"]').click();
-    // Fill input[type="search"]
-    await page.locator('input[type="search"]').fill('strange');
-  
-    await Promise.all([
-      page.waitForResponse(
-        response =>
+  // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
+  await page.locator('[title="Search and Replace"]').click();
+  // Fill input[type="search"]
+  await page.locator('input[type="search"]').fill('strange');
+
+  await Promise.all([
+    page.waitForResponse(
+      response =>
         /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
-          response.request().method() === 'GET'
-      ),
-      page.locator('input[type="search"]').press('Enter'),
-      page.waitForSelector('.jp-search-replace-tab >> .jp-progress', {
-        state: 'hidden'
-      })
-    ]);
-  
-    expect(
-      await page.waitForSelector('jp-tree-view[role="tree"] >> text=55')
-    ).toBeTruthy();
-  
-    const itemMatch = page.locator('.search-tree-files:has-text("test_handlers.py") >> .search-tree-matches');
-    await itemMatch.first().waitFor();
-  
-    await page.locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]').click();
-    await page.locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]').fill('helloqs');
+        response.request().method() === 'GET'
+    ),
+    page.locator('input[type="search"]').press('Enter'),
+    page.waitForSelector('.jp-search-replace-tab >> .jp-progress', {
+      state: 'hidden'
+    })
+  ]);
 
-    // press replace match for a particular match in `test_handlers.py` only
-    await itemMatch.nth(1).locator('[title="button to replace a particular match"]').click();
+  expect(
+    await page.waitForSelector('jp-tree-view[role="tree"] >> text=55')
+  ).toBeTruthy();
 
-    // new results for previous query 'strange' should have one less result in `test_handlers.py`
-    await page.waitForTimeout(800);
-    expect(
-        await page.waitForSelector('jp-tree-view[role="tree"] >> text=54')
-    ).toBeTruthy();
-  
-    // new search with `helloqs`
-    await page.locator('input[type="search"]').fill('helloqs');
-    await Promise.all([
-      page.waitForResponse(
-        response =>
+  const itemMatch = page.locator(
+    '.search-tree-files:has-text("test_handlers.py") >> .search-tree-matches'
+  );
+  await itemMatch.first().waitFor();
+
+  await page
+    .locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]')
+    .click();
+  await page
+    .locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]')
+    .fill('helloqs');
+
+  // press replace match for a particular match in `test_handlers.py` only
+  await itemMatch
+    .nth(1)
+    .locator('[title="button to replace a particular match"]')
+    .click();
+
+  // new results for previous query 'strange' should have one less result in `test_handlers.py`
+  await page.waitForTimeout(800);
+  expect(
+    await page.waitForSelector('jp-tree-view[role="tree"] >> text=54')
+  ).toBeTruthy();
+
+  // new search with `helloqs`
+  await page.locator('input[type="search"]').fill('helloqs');
+  await Promise.all([
+    page.waitForResponse(
+      response =>
         /.*search\/[\w-]+\?query=helloqs/.test(response.url()) &&
-          response.request().method() === 'GET'
-      ),
-      page.locator('input[type="search"]').press('Enter'),
-      page.waitForSelector('.jp-search-replace-tab >> .jp-progress', {
-        state: 'hidden'
-      })
-    ]);
-  
-    // verify if `test_handlers.py` has changed
-    expect(
-      await page.waitForSelector('jp-tree-view[role="tree"] >> text=1')
-    ).toBeTruthy();
-  
-    await expect(itemMatch.first()).toHaveText('                    "line": "Unicode helloqs sub file, very strange\\n",');
+        response.request().method() === 'GET'
+    ),
+    page.locator('input[type="search"]').press('Enter'),
+    page.waitForSelector('.jp-search-replace-tab >> .jp-progress', {
+      state: 'hidden'
+    })
+  ]);
+
+  // verify if `test_handlers.py` has changed
+  expect(
+    await page.waitForSelector('jp-tree-view[role="tree"] >> text=1')
+  ).toBeTruthy();
+
+  await expect(itemMatch.first()).toHaveText(
+    '                    "line": "Unicode helloqs sub file, very strange\\n",'
+  );
 });
--- ui-tests/tests/search.spec.ts
+++ ui-tests/tests/search.spec.ts
@@ -23,7 +23,7 @@ test.afterAll(async ({ baseURL, tmpPath }) => {
 
 test('should get 5 matches', async ({ page }) => {
   // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
-  await page.locator('[title="Search and replace"]').click();
+  await page.locator('[title="Search and Replace"]').click();
   // Fill input[type="search"]
   await page.locator('input[type="search"]').fill('strange');
 
@@ -38,7 +38,7 @@ test('should get 5 matches', async ({ page }) => {
 
   await page.waitForSelector('.jp-search-replace-tab >> .jp-progress', {
     state: 'hidden'
-  })
+  });
 
   expect(
     await page.waitForSelector('jp-tree-view[role="tree"] >> text=5')
@@ -49,19 +49,21 @@ test('should get 5 matches', async ({ page }) => {
   );
 
   await page.locator('jp-tree-item:nth-child(5)').click();
-  await expect(page).toHaveURL('http://localhost:8888/lab/tree/search-replace-test/conftest.py');
+  await expect(page).toHaveURL(
+    'http://localhost:8888/lab/tree/search-replace-test/conftest.py'
+  );
 });
 
 test('should get no matches', async ({ page }) => {
   // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
-  await page.locator('[title="Search and replace"]').click();
+  await page.locator('[title="Search and Replace"]').click();
   // Fill input[type="search"]
   await page.locator('input[type="search"]').fill('dhit');
 
   await Promise.all([
     page.waitForResponse(
       response =>
-      /.*search\/[\w-]+\?query=dhit/.test(response.url()) &&
+        /.*search\/[\w-]+\?query=dhit/.test(response.url()) &&
         response.request().method() === 'GET'
     ),
     page.locator('input[type="search"]').press('Enter')
@@ -74,90 +76,95 @@ test('should get no matches', async ({ page }) => {
 
 test('should test for case sensitive option', async ({ page }) => {
   // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
-  await page.locator('[title="Search and replace"]').click();
+  await page.locator('[title="Search and Replace"]').click();
   // Fill input[type="search"]
   await page.locator('input[type="search"]').fill('Strange');
 
   let response_url: string;
 
   await Promise.all([
-    page.waitForResponse(
-      response => {
-        response_url = response.url();
-        return /.*search\/[\w-]+\?query=Strange/.test(response.url()) &&
+    page.waitForResponse(response => {
+      response_url = response.url();
+      return (
+        /.*search\/[\w-]+\?query=Strange/.test(response.url()) &&
         response.request().method() === 'GET'
-      }
-    ),
+      );
+    }),
     page.locator('input[type="search"]').press('Enter'),
     page.locator('[title="button to enable case sensitive mode"]').click()
   ]);
 
   expect(/case_sensitive=true/.test(response_url)).toEqual(true);
 
-  expect(await page.waitForSelector('jp-tree-view[role="tree"] >> text=1')).toBeTruthy();
+  expect(
+    await page.waitForSelector('jp-tree-view[role="tree"] >> text=1')
+  ).toBeTruthy();
 });
 
 test('should test for whole word option', async ({ page }) => {
   // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
-  await page.locator('[title="Search and replace"]').click();
+  await page.locator('[title="Search and Replace"]').click();
   // Fill input[type="search"]
   await page.locator('input[type="search"]').fill('strange');
 
   let response_url: string;
 
   await Promise.all([
-    page.waitForResponse(
-      response => {
-        response_url = response.url();
-        return /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
+    page.waitForResponse(response => {
+      response_url = response.url();
+      return (
+        /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
         response.request().method() === 'GET'
-      }
-    ),
+      );
+    }),
     page.locator('input[type="search"]').press('Enter'),
     page.locator('[title="button to enable whole word mode"]').click()
   ]);
 
   expect(/whole_word=true/.test(response_url)).toEqual(true);
 
-  expect(await page.waitForSelector('jp-tree-view[role="tree"] >> text=4')).toBeTruthy();
+  expect(
+    await page.waitForSelector('jp-tree-view[role="tree"] >> text=4')
+  ).toBeTruthy();
 });
 
 test('should test for use regex option', async ({ page }) => {
   // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
-  await page.locator('[title="Search and replace"]').click();
+  await page.locator('[title="Search and Replace"]').click();
   // Fill input[type="search"]
   await page.locator('input[type="search"]').fill('str.*');
 
   let response_url: string;
 
   await Promise.all([
-    page.waitForResponse(
-      response => {
-        response_url = response.url();
-        return /.*search\/[\w-]+\?query=str.\*/.test(response.url()) &&
+    page.waitForResponse(response => {
+      response_url = response.url();
+      return (
+        /.*search\/[\w-]+\?query=str.\*/.test(response.url()) &&
         response.request().method() === 'GET'
-      }
-    ),
+      );
+    }),
     page.locator('input[type="search"]').press('Enter'),
     page.locator('[title="button to enable use regex mode"]').click()
   ]);
 
   expect(/use_regex=true/.test(response_url)).toEqual(true);
 
-  expect(await page.waitForSelector('jp-tree-view[role="tree"] >> text=5')).toBeTruthy();
+  expect(
+    await page.waitForSelector('jp-tree-view[role="tree"] >> text=5')
+  ).toBeTruthy();
 });
-
 
 test('should make a new request on refresh', async ({ page }) => {
   // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
-  await page.locator('[title="Search and replace"]').click();
+  await page.locator('[title="Search and Replace"]').click();
   // Fill input[type="search"]
   await page.locator('input[type="search"]').fill('strange');
 
   await Promise.all([
     page.waitForResponse(
       response =>
-      /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
+        /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
         response.request().method() === 'GET'
     ),
     page.locator('input[type="search"]').press('Enter')
@@ -166,24 +173,25 @@ test('should make a new request on refresh', async ({ page }) => {
   await Promise.all([
     page.waitForResponse(
       response =>
-      /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
+        /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
         response.request().method() === 'GET'
     ),
     page.locator('[title="button to refresh and reload results"]').click()
   ]);
 });
 
-
-test('should expand and collapse tree view on clicking expand-collapse button', async ({ page }) => {
+test('should expand and collapse tree view on clicking expand-collapse button', async ({
+  page
+}) => {
   // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
-  await page.locator('[title="Search and replace"]').click();
+  await page.locator('[title="Search and Replace"]').click();
   // Fill input[type="search"]
   await page.locator('input[type="search"]').fill('strange');
 
   await Promise.all([
     page.waitForResponse(
       response =>
-      /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
+        /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
         response.request().method() === 'GET'
     ),
     page.locator('input[type="search"]').press('Enter'),
@@ -194,26 +202,33 @@ test('should expand and collapse tree view on clicking expand-collapse button', 
 
   // added timeouts allowing DOM to update
   await page.waitForTimeout(60);
-  await page.locator('[title="button to expand and collapse all results"]').click();
+  await page
+    .locator('[title="button to expand and collapse all results"]')
+    .click();
   await page.waitForTimeout(60);
-  expect(await page.locator('.search-tree-files').getAttribute('aria-expanded')).toEqual("false");
+  expect(
+    await page.locator('.search-tree-files').getAttribute('aria-expanded')
+  ).toEqual('false');
 
-  await page.locator('[title="button to expand and collapse all results"]').click();
+  await page
+    .locator('[title="button to expand and collapse all results"]')
+    .click();
   await page.waitForTimeout(60);
-  expect(await page.locator('.search-tree-files').getAttribute('aria-expanded')).toEqual("true");
+  expect(
+    await page.locator('.search-tree-files').getAttribute('aria-expanded')
+  ).toEqual('true');
 });
-
 
 test('should replace results on replace-all button', async ({ page }) => {
   // Click #tab-key-0 .lm-TabBar-tabIcon svg >> nth=0
-  await page.locator('[title="Search and replace"]').click();
+  await page.locator('[title="Search and Replace"]').click();
   // Fill input[type="search"]
   await page.locator('input[type="search"]').fill('strange');
 
   await Promise.all([
     page.waitForResponse(
       response =>
-      /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
+        /.*search\/[\w-]+\?query=strange/.test(response.url()) &&
         response.request().method() === 'GET'
     ),
     page.locator('input[type="search"]').press('Enter'),
@@ -230,15 +245,21 @@ test('should replace results on replace-all button', async ({ page }) => {
     '                "Is that Strange enough?",'
   );
 
-  await page.locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]').click();
-  await page.locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]').fill('hello');
-  await page.locator('[title="button to replace all matches with query"]').click();
+  await page
+    .locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]')
+    .click();
+  await page
+    .locator('#jp-search-replace >> text=Replace >> [placeholder="Replace"]')
+    .fill('hello');
+  await page
+    .locator('[title="button to replace all matches with query"]')
+    .click();
 
   await page.locator('input[type="search"]').fill('hello');
   await Promise.all([
     page.waitForResponse(
       response =>
-      /.*search\/[\w-]+\?query=hello/.test(response.url()) &&
+        /.*search\/[\w-]+\?query=hello/.test(response.url()) &&
         response.request().method() === 'GET'
     ),
     page.locator('input[type="search"]').press('Enter'),
