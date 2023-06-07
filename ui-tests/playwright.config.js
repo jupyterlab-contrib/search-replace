c57ddc7299a9ba37305d73c4388ba882b6e24034
Frederic COLLONVAL <fcollonval@users.noreply.github.com>
2023-06-07 08:27:12 +0200

    Try fixing integration test config

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -6,7 +6,7 @@
  module.exports = {
    ...baseConfig,
    webServer: {
-     command: 'jlpm start',
+     command: 'jupyter lab --config jupyter_server_test_config.py',
      url: 'http://localhost:8888/lab',
      timeout: 120 * 1000,
      reuseExistingServer: !process.env.CI,
