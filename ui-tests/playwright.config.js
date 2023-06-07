55fa89c6cceb4889abf4a74a06422d5f0c1d9374
Frédéric Collonval <fcollonval@gmail.com>
2023-06-07 16:41:31 +0200

    Don't start the server from playwright config

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -1,14 +1,4 @@
 /**
  * Configuration for Playwright using default from @jupyterlab/galata
  */
- const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
-
- module.exports = {
-   ...baseConfig,
-   webServer: {
-     command: 'jupyter lab --config jupyter_server_test_config.py',
-     url: 'http://localhost:8888/lab',
-     timeout: 120 * 1000,
-     reuseExistingServer: !process.env.CI,
-   },
-}
+module.exports = require('@jupyterlab/galata/lib/playwright-config');
