a6e0a13f6e883cd41d170623d93dc1ded753bc6a
Frederic COLLONVAL <fcollonval@users.noreply.github.com>
2023-10-10 09:12:33 +0200

    Don't execute server from playwright Apply fix from https://github.com/jupyterlab-contrib/search-replace/pull/89/commits/55fa89c6cceb4889abf4a74a06422d5f0c1d9374

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -1,14 +1,4 @@
 /**
  * Configuration for Playwright using default from @jupyterlab/galata
  */
-const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
-
-module.exports = {
-  ...baseConfig,
-  webServer: {
-    command: 'jlpm start',
-    url: 'http://localhost:8888/lab',
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI
-  }
-};
+module.exports = require('@jupyterlab/galata/lib/playwright-config');
